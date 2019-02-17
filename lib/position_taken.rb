159f1ad27a48aeab3e68ed43da8d473f8cc5c562
John Wan <elite_floral@msn.com>
2019-02-17 19:32:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,6 @@
-# code your #position_taken? method here!
+ #position_taken? in lib/position_taken.rb returns false if the board has an empty string " " in the position
+def position_takes?(board, index)
+  puts board[].inspect
+  puts index.inspect
+end
+array[index_number]==" "
